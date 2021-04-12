8d26a3929dc048220e70de180c32ba8d816464be
hmaas00 <hmaas00@gmail.com>
2021-04-11 23:05:07 -0300

    version to deploy

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,19 +1,26 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": process.env.DEV_USERNAME,
+    "password": process.env.DEV_PASSWORD,
+    "database": process.env.DEV_DATABASE,
+    "host": process.env.DEV_HOST,
     "dialect": "postgres",
-    "aws_region": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_region": process.env.DEV_AWS_REGION,
+    "aws_profile": process.env.DEV_AWS_PROFILE,
+    "aws_media_bucket": process.env.DEV_AWS_MEDIA_BUCKET
   },
   "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
+    "username": process.env.PROD_USERNAME,
+    "password": process.env.PROD_PASSWORD,
+    "database": process.env.PROD_DATABASE,
+    "host": process.env.PROD_HOST,
     "dialect": "postgres"
   }
 }
+//DB instance identifier:database-udagram-dev
+//Master username:postgres
+//Master password:udagram0411
+//port:5432
+//database: udagram
+//region:us-east-1f
+//endpoint: database-udagram-dev.c2t8f5dniwft.us-east-1.rds.amazonaws.com
